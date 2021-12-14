e245bb8b1aee2fcd00bb840361e23815a9b1c373
JayceeNakagawa <60021118+JayceeNakagawa@users.noreply.github.com>
2021-12-13 17:34:31 -0800

    Add files via upload

--- client/CardGameClient.java
+++ client/CardGameClient.java
@@ -11,7 +11,6 @@ public class CardGameClient {
             String response;
             Socket s = new Socket("localhost", 59898);
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            PrintStream ps = new PrintStream(s.getOutputStream());
             BufferedReader brs = new BufferedReader(new InputStreamReader(s.getInputStream()));
             
             DataOutputStream toServer = new DataOutputStream(s.getOutputStream());
@@ -22,19 +21,14 @@ public class CardGameClient {
             while (true) {
                 System.out.println("input data:");
                 String st = br.readLine();  //input
-                //ps.println(st);       //this is also writing to server! Might need to get rid of it.
-                if (st.equals("exit")) {
-                    System.exit(1);
-                }
                 System.out.println("transmitting to server...");
                 toServer.writeBytes(st + "\n");     //Note: the \n is needed because the input in the server will recognize the last entry without it.
                 //what is the server response?
-                //response = brs.readLine();   
-                System.out.println("what the hell is this: " + brs.readLine());
-                //if (brs.readLine() == "You Lose" || brs.readLine() == "Victory" || brs.readLine() == "Tie Game") {
-                //    s.close();
-                //}
-                //System.out.println("server response:" + response);
+                response = brs.readLine();   
+                System.out.println("Server Response: " + response);
+                if (response.equals("You Lose") || response.equals("Victory") || response.equals("Tie Game")) {
+                    break;
+                }
             }
         }
         catch (UnknownHostException e) {
